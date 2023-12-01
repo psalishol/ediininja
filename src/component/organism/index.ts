9fdb30c40028f2c829bb543f3469d300492d07f2
psalishol <psalishol80@gmail.com>
2023-12-01 19:14:38 +0100

    export menu board

--- src/component/organism/index.ts
+++ src/component/organism/index.ts
@@ -1,3 +1,4 @@
 export {default as GameBackground} from './game-background';
 export {default as ScoreBoard} from './score-board';
 export {default as LifeBoard} from './life-board';
+export {default as MenuBoard} from './menu-board';

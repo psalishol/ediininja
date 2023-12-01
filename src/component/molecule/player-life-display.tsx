3062d63a698082a333014f13451a6a8000b108b1
psalishol <psalishol80@gmail.com>
2023-12-01 15:19:37 +0100

    comp: add player life display

---
+++ src/component/molecule/player-life-display.tsx
@@ -0,0 +1,69 @@
+import {useState} from 'react';
+import {View} from 'react-native';
+import {RedXTemplate, XTemplate} from '../../assets/svg';
+import {screenWidth, screenHeight} from '../../consts';
+import {size} from '../../helper';
+import {PlayerLife} from '../../types';
+
+const PlayerLifeDisplay: React.FunctionComponent = () => {
+  const [lifeLeft] = useState<PlayerLife>('3');
+
+  const LIFE_DIMENSION = size(30);
+
+  switch (lifeLeft) {
+    case '1':
+      return (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            position: 'absolute',
+            backgroundColor: 'red',
+            justifyContent: 'space-around',
+            width: screenWidth * 0.11,
+            height: screenHeight * 0.17,
+            right: screenWidth * 0.04,
+          }}>
+          <RedXTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+          <RedXTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+          <XTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+        </View>
+      );
+    case '2':
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          position: 'absolute',
+          backgroundColor: 'red',
+          justifyContent: 'space-around',
+          width: screenWidth * 0.11,
+          height: screenHeight * 0.17,
+          right: screenWidth * 0.04,
+        }}>
+        <RedXTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+        <XTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+        <XTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+      </View>;
+    default:
+      return (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            position: 'absolute',
+            backgroundColor: 'red',
+            justifyContent: 'space-around',
+            width: screenWidth * 0.11,
+            height: screenHeight * 0.17,
+            right: screenWidth * 0.04,
+          }}>
+          <XTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+          <XTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+          <XTemplate height={LIFE_DIMENSION} width={LIFE_DIMENSION} />
+        </View>
+      );
+  }
+};
+
+export default PlayerLifeDisplay;

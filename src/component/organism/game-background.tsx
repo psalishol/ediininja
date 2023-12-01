715157e9863cc5959e59561d69f103f4c722a833
psalishol <psalishol80@gmail.com>
2023-12-01 15:28:39 +0100

    update: change dimension

--- src/component/organism/game-background.tsx
+++ src/component/organism/game-background.tsx
@@ -2,6 +2,7 @@ import {Image, StatusBar, StyleSheet, View} from 'react-native';
 import {backgroundImg} from '../../assets/img';
 import {useEffect} from 'react';
 import SystemNavigationBar from 'react-native-system-navigation-bar';
+import {screenHeight, screenWidth} from '../../consts';
 
 interface Props {
   children: React.ReactNode;
@@ -15,7 +16,11 @@ const GameBackground: React.FunctionComponent<Props> = ({children}) => {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={'transparent'} barStyle={'dark-content'} />
-      <Image style={styles.backgroundImg} source={backgroundImg} />
+      <Image
+        resizeMode="stretch"
+        style={styles.backgroundImg}
+        source={backgroundImg}
+      />
       {children}
     </View>
   );
@@ -29,9 +34,7 @@ const styles = StyleSheet.create({
   },
   backgroundImg: {
     position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
+    height: screenHeight,
+    width: screenWidth * 1.1,
   },
 });

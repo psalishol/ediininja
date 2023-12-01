a09fa463a737d138782295cbb7dc2dd22afa913f
psalishol <psalishol80@gmail.com>
2023-12-01 17:47:37 +0100

    export svgs

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,56 @@
+export {default as ScoreBoardTemplate} from './eddii_ninja_main_menu_base.svg';
+export {default as LeftBoardTemplate} from './eddii_ninja_main_menu_base_left_side.svg';
+export {default as RightBoardTemplate} from './eddii_ninja_main_menu_base_right_side.svg';
+export {default as RedXTemplate} from './eddii_ninja_main_menu_x_red.svg';
+export {default as XTemplate} from './eddii_ninja_main_menu_x.svg';
+export {default as UncutAppleSvg} from './game_ninja_item_apple.svg';
+
+// Food Item Svgs
+
+// Apple
+export {default as AppleCutSvg} from './apple_cut.svg';
+export {default as AppleSvg} from './apple.svg';
+
+// Banana
+export {default as BananaCutSvg} from './banana_cut.svg';
+export {default as BananaSvg} from './banana.svg';
+
+// Broccoli
+export {default as BroccoliCutSvg} from './brocolli_Cut.svg';
+export {default as BroccoliSvg} from './broccoli.svg';
+
+// Pizza
+export {default as PizzaCutSvg} from './pizza_cut.svg';
+export {default as PizzaSvg} from './pizza.svg';
+
+// Carrot
+export {default as CarrotCutSvg} from './carrot_Cut.svg';
+export {default as CarrotSvg} from './carrot.svg';
+
+// Grapes
+export {default as GrapesCutSvg} from './grapes_cut.svg';
+export {default as GrapesSvg} from './grapes.svg';
+
+// French Fries
+export {default as FrenchFriesCutSvg} from './french_fries_cut.svg';
+export {default as FrenchFriesSvg} from './french_fries.svg';
+
+// Hamburger
+export {default as HamburgerCutSvg} from './hamburger_cut.svg';
+export {default as HamburgerSvg} from './hamburger.svg';
+
+// Ice Cream
+export {default as IceCreamCutSvg} from './ice_cream_cut.svg';
+export {default as IceCreamSvg} from './ice_cream.svg';
+
+// Chicken Breast
+export {default as ChickenCutSvg} from './chicken_cut.svg';
+export {default as ChickenSvg} from './chicken.svg';
+
+// Sandwich
+export {default as SandwichCutSvg} from './sandwich_cut.svg';
+export {default as SandwichSvg} from './sandwich.svg';
+
+// Watermelon
+export {default as WatermelonCutSvg} from './watermelon_cut.svg';
+export {default as WatermelonSvg} from './watermelon.svg';

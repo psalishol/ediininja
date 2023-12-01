cd5f9ce64b8d729c4a948ce446ccb5ed1bd29615
psalishol <psalishol80@gmail.com>
2023-12-01 14:52:09 +0100

    update: style slashed fruit

--- src/screen/edii-screen.tsx
+++ src/screen/edii-screen.tsx
@@ -1,4 +1,4 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {Image, SafeAreaView, Text, View} from 'react-native';
 import {GameBackground} from '../component/organism';
 import {
   LeftBoardTemplate,
@@ -9,6 +9,7 @@ import {
 import {screenHeight, screenWidth} from '../consts';
 import {CurrentScore, HighScore} from '../component/atom';
 import {size} from '../helper';
+import {slashImg} from '../assets/img';
 
 const EdiiScreen: React.FunctionComponent = () => {
   return (
@@ -67,16 +68,28 @@ const ScoreBoard: React.FunctionComponent = () => {
       <View
         style={{
           position: 'absolute',
-          marginLeft: screenWidth * 0.153,
-          marginTop: screenHeight * 0.017,
+          marginLeft: screenWidth * 0.152,
+          marginTop: screenHeight * 0.013,
           justifyContent: 'center',
           alignItems: 'center',
           height: 50,
           width: 50,
+          elevation: 40,
+          shadowColor: 'white',
+          shadowRadius: 10,
+          shadowOffset: {width: 2, height: 2},
+          shadowOpacity: 0.9,
+          borderRadius: 100,
         }}>
-        <UncutAppleSvg
-          height={screenHeight * 0.17}
-          width={screenHeight * 0.17}
+        <UncutAppleSvg height={screenHeight * 0.2} width={screenHeight * 0.2} />
+        <Image
+          resizeMode="contain"
+          style={{
+            position: 'absolute',
+            height: 30,
+            transform: [{rotate: '80deg'}],
+          }}
+          source={slashImg}
         />
       </View>
     </View>

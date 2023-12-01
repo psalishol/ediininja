dd2bbc5c39f666144284c808bd1c9976ff0625df
psalishol <psalishol80@gmail.com>
2023-12-01 14:16:06 +0100

    comp: high score count

---
+++ src/component/atom/high-score.tsx
@@ -0,0 +1,30 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {screenHeight, screenWidth} from '../../consts';
+
+const HighScore: React.FunctionComponent = () => {
+  const score = 100000;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.score}>{score}</Text>
+    </View>
+  );
+};
+
+export default HighScore;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    marginLeft: screenWidth * 0.037,
+    marginTop: screenHeight * 0.048,
+    height: screenHeight * 0.07,
+    width: screenWidth * 0.1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  score: {
+    fontWeight: '900',
+    fontSize: 20,
+    color: 'white',
+  },
+});

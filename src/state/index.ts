3c8c729388bd538995a53ff87815178310748029
psalishol <psalishol80@gmail.com>
2023-12-02 17:32:10 +0100

    update: add start game atom

--- src/state/index.ts
+++ src/state/index.ts
@@ -2,3 +2,6 @@ import {atom} from 'jotai';
 
 // holds state for either game over or not. defaults to (false)
 export const gameOverAtom = atom<boolean>(false);
+
+// Start that holds value for if the game is started or not.
+export const startGameAtom = atom<boolean>(false);

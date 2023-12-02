d1c722526f962923fd7e822426ae2bdad514424d
psalishol <psalishol80@gmail.com>
2023-12-02 17:33:07 +0100

    update: add scores atom

--- src/state/index.ts
+++ src/state/index.ts
@@ -5,3 +5,7 @@ export const gameOverAtom = atom<boolean>(false);
 
 // Start that holds value for if the game is started or not.
 export const startGameAtom = atom<boolean>(false);
+
+export const highScoreAtom = atom<number>(0);
+
+export const currentScoreAtom = atom<number>(0);

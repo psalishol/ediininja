1164437021bb5d904b67f45bc4a0a00c56db71e0
psalishol <psalishol80@gmail.com>
2023-12-03 09:21:07 +0100

    update: export lobby high score text

--- src/component/atom/index.ts
+++ src/component/atom/index.ts
@@ -1,3 +1,4 @@
 export {default as HighScore} from './high-score';
 export {default as CurrentScore} from './current-score';
 export {default as CountDownToStartText} from './countdown-to-start-text';
+export {default as LobbyHighScoreText} from './lobby-high-score-text';

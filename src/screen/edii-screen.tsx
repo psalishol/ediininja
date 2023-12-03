4f8478000e775078f5b89feca524df8e7da7356e
psalishol <psalishol80@gmail.com>
2023-12-03 15:12:53 +0100

    update: slice food -- logic not implemented

--- src/screen/edii-screen.tsx
+++ src/screen/edii-screen.tsx
@@ -1,4 +1,4 @@
-import {View} from 'react-native';
+import {Animated, Image, View} from 'react-native';
 import {
   GameBackground,
   LifeBoard,
@@ -14,12 +14,17 @@ import {
   gameCountdownStartAtom,
 } from '../state';
 import {useAtom, useSetAtom} from 'jotai';
-import {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {CountDownToStartText} from '../component/atom';
 import {EdiiLobbyLayout} from '../layout';
 import {FoodItems} from '../types';
 import {randomID, size} from '../helper';
 import {renderFood} from '../util/food-render';
+import SoundPlayer from 'react-native-sound-player';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import {runOnJS} from 'react-native-reanimated';
+import {slashImg} from '../assets/img';
+import {StartGameButton} from '../component/molecule';
 
 const EdiiScreen: React.FunctionComponent = () => {
   const [startGame] = useAtom(startGameAtom);
@@ -146,22 +151,32 @@ interface Props {
 const ProjectedFood: React.FunctionComponent<Props> = ({food}) => {
   const {foodItem, id, point} = food;
 
+  const [sliced, setSliced] = useState<boolean>(false);
+
+  const handleSliceFood = useCallback(() => {
+    setSliced(prev => !prev);
+  }, []);
+
+  console.log('sliced', sliced);
+
   return (
-    <FoodItemRotationContainer>
-      {renderFood(foodItem, false, size(100))}
+    <FoodItemRotationContainer onSlice={handleSliceFood}>
+      {renderFood(foodItem, sliced, size(100))}
     </FoodItemRotationContainer>
   );
 };
 
 interface ContainerProp {
   children: React.ReactNode;
+  onSlice: () => void;
 }
 
 const FoodItemRotationContainer: React.FunctionComponent<ContainerProp> = ({
   children,
+  onSlice,
 }) => {
   return (
-    <FoodItemTranslationContainer>
+    <FoodItemTranslationContainer onSlice={onSlice}>
       <MotiView
         from={{rotate: '0deg'}}
         animate={{rotate: '360deg'}}
@@ -185,6 +200,7 @@ const FoodItemRotationContainer: React.FunctionComponent<ContainerProp> = ({
 
 const FoodItemTranslationContainer: React.FunctionComponent<ContainerProp> = ({
   children,
+  onSlice,
 }) => {
   const [animatedUp, setAnimatedUp] = useState<boolean>(false);
 
@@ -214,6 +230,63 @@ const FoodItemTranslationContainer: React.FunctionComponent<ContainerProp> = ({
       onDidAnimate={handleFinishAnimatingUp}
       transition={{type: 'timing', duration: FOOD_TRANSLATION_DURATION}}>
       {children}
+      <SliceFoodComp onSlice={onSlice} />
     </MotiView>
+  );
+};
+
+interface SliceFoodProp {
+  onSlice?: () => void;
+}
+
+const SliceFoodComp: React.FunctionComponent<SliceFoodProp> = ({onSlice}) => {
+  const [sliced, setSliced] = useState<boolean>(false);
+  // const [startingGame, setStartingGame] = useAtom(startingGameAtom);
+
+  const handleGameStarted = async () => {
+    try {
+      onSlice && onSlice();
+
+      setSliced(true);
+
+      // play slash sound
+      SoundPlayer.playSoundFile('slash2', 'mpeg');
+    } catch (error) {
+      console.log('error cutting food', error);
+    }
+  };
+
+  const gesture = Gesture.Pan().onEnd(_e => {
+    runOnJS(handleGameStarted)();
+  });
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <Animated.View
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+          width: size(100),
+          // height: size(100),
+          // backgroundColor: 'red',
+          alignSelf: 'baseline',
+        }}>
+        {sliced && (
+          <MotiView
+            from={{opacity: 1, translateX: -size(20), translateY: size(20)}}
+            animate={{opacity: 0, translateX: size(50), translateY: -size(30)}}
+            style={{position: 'absolute'}}>
+            <Image
+              style={{height: size(120), width: size(120), marginTop: size(30)}}
+              resizeMode="contain"
+              source={slashImg}
+            />
+          </MotiView>
+        )}
+      </Animated.View>
+    </GestureDetector>
   );
 };

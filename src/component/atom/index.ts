c45030f9095839505aafe2b353a3242022d7b68b
psalishol <psalishol80@gmail.com>
2023-12-03 09:08:13 +0100

    update: export countdown to start text

--- src/component/atom/index.ts
+++ src/component/atom/index.ts
@@ -1,2 +1,3 @@
 export {default as HighScore} from './high-score';
 export {default as CurrentScore} from './current-score';
+export {default as CountDownToStartText} from './countdown-to-start-text';

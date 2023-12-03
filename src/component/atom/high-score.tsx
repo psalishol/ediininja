0400f6b098563d3b132506659aa2f60e16ef5fa8
psalishol <psalishol80@gmail.com>
2023-12-03 02:39:43 +0100

    update: switch to scaling text comp

--- src/component/atom/high-score.tsx
+++ src/component/atom/high-score.tsx
@@ -2,12 +2,13 @@ import {StyleSheet, Text, View} from 'react-native';
 import {screenHeight, screenWidth} from '../../consts';
 import {highScoreAtom} from '../../state';
 import {useAtomValue} from 'jotai';
+import ScalingScoreText from './scaling-score-text';
 
 const HighScore: React.FunctionComponent = () => {
   const score = useAtomValue(highScoreAtom);
   return (
     <View style={styles.container}>
-      <Text style={styles.score}>{score}</Text>
+      <ScalingScoreText value={score} />
     </View>
   );
 };
@@ -23,10 +24,5 @@ const styles = StyleSheet.create({
     width: screenWidth * 0.1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  score: {
-    fontWeight: '900',
-    fontSize: 20,
-    color: 'white',
   },
 });

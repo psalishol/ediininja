51235259911feff05938f41f351462def471471a
psalishol <psalishol80@gmail.com>
2023-12-03 20:56:47 +0100

    update: set last sliced food

--- src/component/molecule/food.tsx
+++ src/component/molecule/food.tsx
@@ -1,8 +1,12 @@
-import {useAtom} from 'jotai';
+import {useAtom, useSetAtom} from 'jotai';
 import React, {useState, useCallback, memo} from 'react';
 import {FoodRotationContainer, FoodTranslationContainer} from '../../container';
 import {size} from '../../helper';
-import {highScoreAtom, currentScoreAtom} from '../../state';
+import {
+  highScoreAtom,
+  currentScoreAtom,
+  lastSlicedFoodItemAtom,
+} from '../../state';
 import {renderFood} from '../../util';
 import {FoodBuilder} from '../../types';
 
@@ -16,6 +20,8 @@ const Food: React.FunctionComponent<Props> = ({food, onFinishTranslation}) => {
 
   const [sliced, setSliced] = useState<boolean>(false);
 
+  const setLastSlicedFood = useSetAtom(lastSlicedFoodItemAtom);
+
   const [highScore, setHighScore] = useAtom(highScoreAtom);
   const [currentScore, setCurrentScore] = useAtom(currentScoreAtom);
 
@@ -25,6 +31,8 @@ const Food: React.FunctionComponent<Props> = ({food, onFinishTranslation}) => {
     const newScore = currentScore + point;
 
     setCurrentScore(newScore);
+
+    setLastSlicedFood(foodItem);
 
     if (newScore > highScore) {
       setHighScore(newScore);

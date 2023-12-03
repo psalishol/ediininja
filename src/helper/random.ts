bd7851ede05f2e25d99adaba9667f6f8ac2d1707
psalishol <psalishol80@gmail.com>
2023-12-03 15:58:01 +0100

    update: add comment & func random int - generates a random number between 0 and the max number.

--- src/helper/random.ts
+++ src/helper/random.ts
@@ -1,5 +1,12 @@
 import uuid from 'react-native-uuid';
 
+// randomID generates random unique strings.
+// eg "jshj32-323jjj2-j23jgjhs-jjshdjh". it is always unique
 export const randomID = (): string => {
   return uuid.v4() as string;
+};
+
+// randomInt generates a random number between 0 and the max number.
+export const randomInt = (max: number): number => {
+  return Math.floor(Math.random() * max);
 };

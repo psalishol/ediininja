786eb156b1bc849d2e31b615c3a6e643e2a6530e
psalishol <psalishol80@gmail.com>
2023-12-03 15:37:00 +0100

    update: change name and import dir

--- src/component/molecule/index.ts
+++ src/component/molecule/index.ts
@@ -1,4 +1,4 @@
-export {default as SlashedFruit} from './slashed-fruit';
+export {default as LastSlicedFood} from './last-sliced-food';
 export {default as PlayerLifeDisplay} from './player-life-display';
 export {default as StartGameButton} from './start-game-button';
 export {default as LobbySliceApple} from './lobby-slice-apple';

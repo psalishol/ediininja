795566e897d7bb4d105102e651a3d8a682c20802
psalishol <psalishol80@gmail.com>
2023-12-03 14:42:18 +0100

    update: implement food translation

--- src/screen/edii-screen.tsx
+++ src/screen/edii-screen.tsx
@@ -6,7 +6,7 @@ import {
   ScoreBoard,
 } from '../component/organism';
 
-import {screenHeight} from '../consts';
+import {screenHeight, foodLibrary, screenWidth} from '../consts';
 import {MotiView} from 'moti';
 import {
   startingGameAtom,
@@ -14,9 +14,12 @@ import {
   gameCountdownStartAtom,
 } from '../state';
 import {useAtom, useSetAtom} from 'jotai';
-import {useEffect} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import {CountDownToStartText} from '../component/atom';
 import {EdiiLobbyLayout} from '../layout';
+import {FoodItems} from '../types';
+import {randomID, size} from '../helper';
+import {renderFood} from '../util/food-render';
 
 const EdiiScreen: React.FunctionComponent = () => {
   const [startGame] = useAtom(startGameAtom);
@@ -41,7 +44,8 @@ const EdiiScreen: React.FunctionComponent = () => {
       {!startGame && !gameCountdownStart && <EdiiLobbyLayout />}
       {/* Game header */}
       {startGame && !gameCountdownStart && (
-        <View>
+        <View style={{flex: 1}}>
+          <FoodProjectionView />
           <MotiView
             from={{translateY: -screenHeight * 0.3}}
             animate={{translateY: 0}}
@@ -77,3 +81,139 @@ const EdiiScreen: React.FunctionComponent = () => {
 };
 
 export default EdiiScreen;
+
+const randomInt = (max: number): number => {
+  return Math.floor(Math.random() * max);
+};
+
+interface FoodBuilder {
+  id: string;
+  foodItem: FoodItems;
+  point: number;
+}
+
+const FoodProjectionView: React.FunctionComponent = () => {
+  const [food, setFood] = useState<FoodBuilder[]>([]);
+
+  //   useEffect(() => {
+  //     const totalFoodItemCount = foodLibrary.length;
+
+  //     const interval = setInterval(() => {
+  //       let foodBuilder: FoodBuilder[] = [];
+
+  //       for (let i = 0; i < randomInt(4); i++) {
+  //         const randFood: FoodBuilder = {
+  //           ...foodLibrary[randomInt(totalFoodItemCount)],
+  //           id: randomID(),
+  //         };
+  //         foodBuilder.push(randFood);
+  //       }
+
+  //       setFood(prev => [...prev, ...foodBuilder]);
+  //     }, 1500);
+
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }, []);
+
+  console.log('food', food);
+
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        // backgroundColor: 'red',
+      }}>
+      <ProjectedFood
+        food={{
+          ...foodLibrary[1],
+          id: randomID(),
+        }}
+      />
+    </View>
+  );
+};
+
+interface Props {
+  food: FoodBuilder;
+}
+
+const ProjectedFood: React.FunctionComponent<Props> = ({food}) => {
+  const {foodItem, id, point} = food;
+
+  return (
+    <FoodItemRotationContainer>
+      {renderFood(foodItem, false, size(100))}
+    </FoodItemRotationContainer>
+  );
+};
+
+interface ContainerProp {
+  children: React.ReactNode;
+}
+
+const FoodItemRotationContainer: React.FunctionComponent<ContainerProp> = ({
+  children,
+}) => {
+  return (
+    <FoodItemTranslationContainer>
+      <MotiView
+        from={{rotate: '0deg'}}
+        animate={{rotate: '360deg'}}
+        transition={{
+          loop: true,
+          type: 'timing',
+          duration: 2000,
+          repeatReverse: false,
+        }}
+        style={{
+          marginTop: size(40),
+          elevation: 50,
+          // backgroundColor: 'blue',
+          alignSelf: 'baseline',
+        }}>
+        {children}
+      </MotiView>
+    </FoodItemTranslationContainer>
+  );
+};
+
+const FoodItemTranslationContainer: React.FunctionComponent<ContainerProp> = ({
+  children,
+}) => {
+  const [animatedUp, setAnimatedUp] = useState<boolean>(false);
+
+  // This is the duration taken for the food to translate up.
+  // TODO: make the duration random between 1000 to 3000
+  const FOOD_TRANSLATION_DURATION = 4000; // 2secs
+
+  const handleFinishAnimatingUp = useCallback(() => {
+    if (animatedUp) {
+      // TODO: remove food from projected map.
+      // decrease the life, because the food was not sliced.
+      console.log('finished animating');
+    } else {
+      setAnimatedUp(true);
+    }
+  }, [animatedUp]);
+
+  console.log('animated up', animatedUp);
+
+  return (
+    <MotiView
+      from={{translateY: screenHeight, translateX: 0}}
+      animate={{
+        translateY: animatedUp ? screenHeight : 0,
+        translateX: animatedUp ? screenWidth * 0.2 : screenWidth * 0.1,
+      }}
+      onDidAnimate={handleFinishAnimatingUp}
+      transition={{type: 'timing', duration: FOOD_TRANSLATION_DURATION}}>
+      {children}
+    </MotiView>
+  );
+};

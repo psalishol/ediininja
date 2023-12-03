46de5834b8969d60be3b26c61991c04201c3820d
psalishol <psalishol80@gmail.com>
2023-12-03 11:47:00 +0100

    update: export quit game button

--- src/component/molecule/index.ts
+++ src/component/molecule/index.ts
@@ -3,4 +3,5 @@ export {default as PlayerLifeDisplay} from './player-life-display';
 export {default as StartGameButton} from './start-game-button';
 export {default as LobbySliceApple} from './lobby-slice-apple';
 export {default as ResumeGameButton} from './resume-game-button';
+export {default as QuitGameButton} from './quit-game-button';
 export * from './basic';

d6f45a7660a46d61cc535dd6ca39153aa78f7519
psalishol <psalishol80@gmail.com>
2023-12-03 09:26:34 +0100

    update: export ninja text

--- src/component/atom/index.ts
+++ src/component/atom/index.ts
@@ -2,3 +2,4 @@ export {default as HighScore} from './high-score';
 export {default as CurrentScore} from './current-score';
 export {default as CountDownToStartText} from './countdown-to-start-text';
 export {default as LobbyHighScoreText} from './lobby-high-score-text';
+export {default as LobbyNinjaText} from './lobby-ninja-text'

62dc85c283976d83d6790ab3de1e24f60d11a2a8
psalishol <psalishol80@gmail.com>
2023-12-03 09:36:23 +0100

    update: rename lobby comp && refactor

--- src/screen/edii-screen.tsx
+++ src/screen/edii-screen.tsx
@@ -35,7 +35,8 @@ import {
   LobbyNinjaText,
   SliceFruitText,
 } from '../component/atom';
-import { LobbySliceApple, StartGameButton } from '../component/molecule';
+import {LobbySliceApple, StartGameButton} from '../component/molecule';
+import { EdiiLobbyLayout } from '../layout';
 
 const EdiiScreen: React.FunctionComponent = () => {
   const [startGame] = useAtom(startGameAtom);
@@ -59,7 +60,7 @@ const EdiiScreen: React.FunctionComponent = () => {
 
   return (
     <GameBackground>
-      {!startGame && !gameCountdownStart && <Lobby />}
+      {!startGame && !gameCountdownStart && <EdiiLobbyLayout />}
       {/* Game header */}
       {startGame && !gameCountdownStart && (
         <View>
@@ -108,49 +109,3 @@ const EdiiScreen: React.FunctionComponent = () => {
 };
 
 export default EdiiScreen;
-
-const Lobby: React.FunctionComponent = () => {
-  return (
-    <View
-      style={{
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(0,0,0,0.6)',
-        padding: size(20),
-      }}>
-      <PopoverBoardSvg />
-
-      <View
-        style={{
-          position: 'absolute',
-          marginLeft: screenWidth * 0.6,
-          marginTop: screenHeight * 0.65,
-        }}>
-        <PopoverNinja height={size(100)} width={100} />
-      </View>
-
-      <View
-        style={{
-          position: 'absolute',
-          marginTop: size(70),
-          width: screenWidth * 0.6,
-          justifyContent: 'center',
-          alignSelf: 'center',
-        }}>
-        <LobbyNinjaText />
-
-        <LobbySliceApple />
-
-        <StartGameButton />
-
-        <SliceFruitText />
-
-        <LobbyHighScoreText />
-      </View>
-    </View>
-  );
-};
-

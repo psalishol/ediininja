93750afea8fd1326b9b3adad43040a8911000fe9
psalishol <psalishol80@gmail.com>
2023-12-03 11:38:31 +0100

    update: refactor resume game button

--- src/layout/menu-layout.tsx
+++ src/layout/menu-layout.tsx
@@ -1,5 +1,5 @@
 import {Text, TouchableOpacity, View} from 'react-native';
-import {Liftview} from '../component/molecule';
+import {Liftview, ResumeGameButton} from '../component/molecule';
 import {CloseSvg, PopoverBoardSvg, PopoverNinja} from '../assets/svg';
 import {screenHeight, screenWidth} from '../consts';
 import {MotiView} from 'moti';
@@ -81,29 +81,7 @@ const MenuLayout: React.FunctionComponent = () => {
 
 export default MenuLayout;
 
-const ResumeGameButton: React.FunctionComponent = () => {
-  const BOX_HEIGHT = screenHeight * 0.1;
-  const BOX_WIDTH = screenWidth * 0.3;
-  return (
-    <TouchableOpacity
-      activeOpacity={0.8}
-      style={{
-        position: 'absolute',
-        backgroundColor: '#1AD3FF',
-        width: BOX_WIDTH,
-        height: BOX_HEIGHT,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: size(10),
-        marginTop: screenHeight * 0.37,
-      }}>
-      <Text style={{color: 'white', fontSize: size(30), fontWeight: '500'}}>
-        Resume
-      </Text>
-    </TouchableOpacity>
-  );
-};
+
 
 const QuitGameButton: React.FunctionComponent = () => {
   const BOX_HEIGHT = screenHeight * 0.1;

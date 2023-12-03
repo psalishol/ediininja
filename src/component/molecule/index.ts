e35c27915c26307f21545c80eb685507de251f43
psalishol <psalishol80@gmail.com>
2023-12-03 11:42:36 +0100

    update: export resume game button

--- src/component/molecule/index.ts
+++ src/component/molecule/index.ts
@@ -2,4 +2,5 @@ export {default as SlashedFruit} from './slashed-fruit';
 export {default as PlayerLifeDisplay} from './player-life-display';
 export {default as StartGameButton} from './start-game-button';
 export {default as LobbySliceApple} from './lobby-slice-apple';
+export {default as ResumeGameButton} from './resume-game-button';
 export * from './basic';

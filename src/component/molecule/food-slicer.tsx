70b1b219f5bdc18473c2dd3fa23bc8c5344875d0
psalishol <psalishol80@gmail.com>
2023-12-03 20:27:35 +0100

    update: memoize comp & fix bug

--- src/component/molecule/food-slicer.tsx
+++ src/component/molecule/food-slicer.tsx
@@ -1,11 +1,12 @@
 import {MotiView} from 'moti';
-import {useState} from 'react';
-import {Animated, Image} from 'react-native';
+import {memo, useState} from 'react';
+import {Image} from 'react-native';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 import {runOnJS} from 'react-native-reanimated';
 import SoundPlayer from 'react-native-sound-player';
 import {slashImg} from '../../assets/img';
 import {size} from '../../helper';
+import Animated from 'react-native-reanimated';
 
 interface Props {
   onSlice?: () => void;
@@ -13,9 +14,8 @@ interface Props {
 
 const FoodSlicer: React.FunctionComponent<Props> = ({onSlice}) => {
   const [sliced, setSliced] = useState<boolean>(false);
-  // const [startingGame, setStartingGame] = useAtom(startingGameAtom);
 
-  const handleGameStarted = async () => {
+  const handleSliceFood = async () => {
     try {
       onSlice && onSlice();
 
@@ -29,33 +29,35 @@ const FoodSlicer: React.FunctionComponent<Props> = ({onSlice}) => {
   };
 
   const gesture = Gesture.Pan().onEnd(_e => {
-    runOnJS(handleGameStarted)();
+    runOnJS(handleSliceFood)();
   });
-
-  if (sliced) {
-    return <></>;
-  }
 
   return (
     <GestureDetector gesture={gesture}>
       <Animated.View
         style={{
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          left: 0,
-          bottom: 0,
+          height: size(150),
           width: size(150),
-          alignSelf: 'baseline',
-          // backgroundColor: 'red',
+          position: 'absolute',
+          alignSelf: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}>
         {sliced && (
           <MotiView
             from={{opacity: 1, translateX: -size(20), translateY: size(20)}}
-            animate={{opacity: 0, translateX: size(50), translateY: -size(30)}}
+            animate={{
+              opacity: 0,
+              translateX: sliced ? size(50) : -size(20),
+              translateY: sliced ? -size(30) : size(20),
+            }}
             style={{position: 'absolute'}}>
             <Image
-              style={{height: size(120), width: size(120), marginTop: size(30)}}
+              style={{
+                height: size(120),
+                width: size(120),
+                marginTop: size(30),
+              }}
               resizeMode="contain"
               source={slashImg}
             />
@@ -66,4 +68,4 @@ const FoodSlicer: React.FunctionComponent<Props> = ({onSlice}) => {
   );
 };
 
-export default FoodSlicer;
+export default memo<Props>(FoodSlicer);

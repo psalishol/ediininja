9da59df75b809c69de0a18959acb612e2379bffe
psalishol <psalishol80@gmail.com>
2023-12-03 09:33:01 +0100

    update: export lobby slice aple

--- src/component/molecule/index.ts
+++ src/component/molecule/index.ts
@@ -1,4 +1,5 @@
 export {default as SlashedFruit} from './slashed-fruit';
 export {default as PlayerLifeDisplay} from './player-life-display';
 export {default as StartGameButton} from './start-game-button';
+export {default as LobbySliceApple} from './lobby-slice-apple';
 export * from './basic';

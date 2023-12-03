7e434f8f1e75806412058e420fc460f92676ee4a
psalishol <psalishol80@gmail.com>
2023-12-03 09:30:56 +0100

    update: export start game button

--- src/component/molecule/index.ts
+++ src/component/molecule/index.ts
@@ -1,3 +1,4 @@
 export {default as SlashedFruit} from './slashed-fruit';
 export {default as PlayerLifeDisplay} from './player-life-display';
+export {default as StartGameButton} from './start-game-button';
 export * from './basic';

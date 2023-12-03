feede66729fa1244500a05fffa7bc32f857dfc88
psalishol <psalishol80@gmail.com>
2023-12-03 16:01:26 +0100

    update: export food renderer

--- src/component/organism/index.ts
+++ src/component/organism/index.ts
@@ -2,3 +2,4 @@ export {default as GameBackground} from './game-background';
 export {default as ScoreBoard} from './score-board';
 export {default as LifeBoard} from './life-board';
 export {default as MenuBoard} from './menu-board';
+export {default as FoodRenderer} from './food-renderer';

a6529c83b7df42db1748503e419101487535fc10
psalishol <psalishol80@gmail.com>
2023-12-03 09:27:09 +0100

    update: export slice fruit text

--- src/component/atom/index.ts
+++ src/component/atom/index.ts
@@ -2,4 +2,5 @@ export {default as HighScore} from './high-score';
 export {default as CurrentScore} from './current-score';
 export {default as CountDownToStartText} from './countdown-to-start-text';
 export {default as LobbyHighScoreText} from './lobby-high-score-text';
-export {default as LobbyNinjaText} from './lobby-ninja-text'
+export {default as LobbyNinjaText} from './lobby-ninja-text';
+export {default as SliceFruitText} from './slice-fruit-text';

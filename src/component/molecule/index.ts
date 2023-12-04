53522eb23681a6401a8da25758ec06fc1ce03517
psalishol <psalishol80@gmail.com>
2023-12-04 13:11:51 +0100

    update: export close game menu button

--- src/component/molecule/index.ts
+++ src/component/molecule/index.ts
@@ -7,4 +7,5 @@ export {default as QuitGameButton} from './quit-game-button';
 export {default as FoodSlicer} from './food-slicer';
 export {default as Food} from './food';
 export {default as CloseGameOverModalButton} from './close-game-over-modal-button';
+export {default as CloseGameMenuButton} from './close-game-menu-button';
 export * from './basic';

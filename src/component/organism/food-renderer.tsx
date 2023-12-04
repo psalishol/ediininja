09ccfc8a13a47da1e27645befde9ac1913c8fdde
psalishol <psalishol80@gmail.com>
2023-12-04 12:00:03 +0100

    update: implement stopping adding new food batch when game paused

--- src/component/organism/food-renderer.tsx
+++ src/component/organism/food-renderer.tsx
@@ -4,33 +4,42 @@ import {foodLibrary} from '../../consts';
 import {randomID, randomInt, size} from '../../helper';
 import {FoodBuilder} from '../../types';
 import {Food} from '../molecule';
+import {gameOverAtom, openGameMenuAtom} from '../../state';
+import {useAtomValue} from 'jotai';
 
 const FoodRenderer: React.FunctionComponent = () => {
   const [foods, setFood] = useState<FoodBuilder[]>([]);
+
+  const gameOver = useAtomValue(gameOverAtom);
+  const openedGameMenu = useAtomValue(openGameMenuAtom);
+
+  const gamePaused = gameOver || openedGameMenu;
+
+  const NEW_FOOD_BATCH_DURATION = 3500; // add new food at the interval of 3.5 sec
 
   useEffect(() => {
     const totalFoodItemCount = foodLibrary.length;
 
     const addFoodBatch = () => {
-      let foodBuilder: FoodBuilder[] = [];
+      if (!gamePaused) {
+        let foodBuilder: FoodBuilder[] = [];
 
-      for (let i = 0; i < randomInt(4); i++) {
-        const randFood: FoodBuilder = {
-          ...foodLibrary[randomInt(totalFoodItemCount)],
-          id: randomID(),
-        };
-        foodBuilder.push(randFood);
+        for (let i = 0; i < randomInt(4); i++) {
+          const randFood: FoodBuilder = {
+            ...foodLibrary[randomInt(totalFoodItemCount)],
+            id: randomID(),
+          };
+          foodBuilder.push(randFood);
+        }
+
+        setFood(prev => [...prev, ...foodBuilder]);
       }
 
-      setFood(prev => [...prev, ...foodBuilder]);
-
-      setTimeout(addFoodBatch, 3500);
+      setTimeout(addFoodBatch, NEW_FOOD_BATCH_DURATION);
     };
 
     addFoodBatch();
-
-    return () => {};
-  }, []);
+  }, [gamePaused]);
 
   const removeFood = useCallback((id: string) => {
     setFood(prev => prev.filter(f => f.id !== id));

be8e2e6e968115c97bf0d5e3afb36175e0ff0261
psalishol <psalishol80@gmail.com>
2023-12-04 15:32:11 +0100

    comp: add sliced food point text

---
+++ src/component/atom/slice-food-point-text.tsx
@@ -0,0 +1,33 @@
+import {MotiText} from 'moti';
+import React, {useCallback, useState} from 'react';
+import {linearEasing} from '../../consts';
+import {size} from '../../helper';
+
+interface Props {
+  point: number;
+}
+
+const SlicedFoodPointText: React.FunctionComponent<Props> = () => {
+  const [animated, setAnimated] = useState<boolean>(false);
+
+  const handleAnimated = useCallback(() => {
+    setAnimated(true);
+  }, []);
+
+  if (animated) {
+    return <></>;
+  }
+
+  return (
+    <MotiText
+      onDidAnimate={handleAnimated}
+      from={{translateY: size(40), opacity: 0}}
+      animate={{translateY: 0, opacity: 1}}
+      transition={{type: 'timing', duration: 1000, easing: linearEasing}}
+      style={{color: 'white', position: 'absolute', fontSize: size(30)}}>
+      +5
+    </MotiText>
+  );
+};
+
+export default SlicedFoodPointText;

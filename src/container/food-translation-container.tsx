9762993d26b04bd7c56cce6a2fd9c5d92add9cc9
psalishol <psalishol80@gmail.com>
2023-12-05 15:15:17 +0100

    update: add animation stop indefinitely && slice animation

--- src/container/food-translation-container.tsx
+++ src/container/food-translation-container.tsx
@@ -5,6 +5,7 @@ import {randomInt} from '../helper';
 import {gameOverAtom, openGameMenuAtom, playerLifeAtom} from '../state';
 import {useAtom, useAtomValue} from 'jotai';
 import Animated, {
+  Easing,
   runOnJS,
   useAnimatedStyle,
   useSharedValue,
@@ -42,6 +43,7 @@ const FoodTranslationContainer: React.FunctionComponent<Props> = ({
   const [playerLife, setPlayerLife] = useAtom(playerLifeAtom);
 
   const FOOD_TRANSLATION_DURATION = randomInt(4000, 3000); // randomDuration between 2secs and 4secs
+  const SLICE_FALL_DURATION = 2000;
 
   const handleSlice = useCallback(() => {
     setSliced(true);
@@ -52,19 +54,24 @@ const FoodTranslationContainer: React.FunctionComponent<Props> = ({
     setAnimatedUp(true);
   };
 
+  // onFinishedTranslation sets the player life in the case player misses
+  // any food. and also set the game over if the life is depleted.
   const onFinishedTranslation = async () => {
     try {
       if (!sliced) {
-        if (playerLife === '1') {
+        if (playerLife === '0') {
+          console.log('game over ');
+          setPlayerLife('0');
           setGameOver(true);
-
           SoundPlayer.playSoundFile('gameover2', 'mp3');
         } else {
           setPlayerLife(prev => {
             if (prev === '3') {
               return '2';
+            } else if (prev === '2') {
+              return '1';
             }
-            return '1';
+            return '0';
           });
         }
       }
@@ -73,23 +80,40 @@ const FoodTranslationContainer: React.FunctionComponent<Props> = ({
     }
   };
 
-  const handleFinishAnimatingUp = () => {
+  // handleFinishAnimatingUp is the callback ran whenever the animation finishes.
+  // This callback is used in two animation controller -> sliced animation and the normal translate animation.
+  const handleFinishAnimatingUp = (_fromSliceAnim: boolean) => {
     'worklet';
-    if (animatedUp) {
-      runOnJS(onFinishedTranslation)();
+    // checks if the animation is from the slice anim. if yes it only runs
+    // the on finish callback which removes the food from the foods array
+    if (_fromSliceAnim) {
       runOnJS(onFinishAnimation)();
     } else {
-      runOnJS(handleAnimatedUp)();
+      // if not from slice anim, then check if the animation just finished translating up
+      // or maybe it hasnt animated up yet.
+      if (animatedUp) {
+        runOnJS(onFinishedTranslation)();
+        runOnJS(onFinishAnimation)();
+      } else {
+        runOnJS(handleAnimatedUp)();
+      }
     }
   };
 
   useEffect(() => {
+    const TRANSLATION_EASING = animatedUp
+      ? Easing.bezier(0.32, 0, 0.67, 0)
+      : Easing.bezier(0.5, 1, 0.89, 1);
+
     const translateY = withTiming(
       animatedUp ? 1 : 0,
-      {duration: FOOD_TRANSLATION_DURATION},
+      {
+        duration: FOOD_TRANSLATION_DURATION,
+        easing: TRANSLATION_EASING,
+      },
       finished => {
         if (finished) {
-          handleFinishAnimatingUp();
+          handleFinishAnimatingUp(false);
         }
       },
     );
@@ -98,13 +122,37 @@ const FoodTranslationContainer: React.FunctionComponent<Props> = ({
       duration: FOOD_TRANSLATION_DURATION,
     });
 
+    const _stopTranslateXAnim = withTiming(translationX.value);
+    const _stopTranslateYAnim = withTiming(translationY.value);
+
+    const _slicedTranslateXAnim = withTiming(translationX.value);
+    const _slicedTranslateYAnim = withTiming(
+      1,
+      {
+        duration: SLICE_FALL_DURATION,
+      },
+      finished => {
+        if (finished) {
+          handleFinishAnimatingUp(true);
+        }
+      },
+    );
+
     const pauseTranslation = gameOver || gamePaused;
 
-    if (!pauseTranslation) {
-      translationY.value = translateY;
-      translationX.value = translateX;
+    if (sliced) {
+      translationY.value = _slicedTranslateYAnim;
+      translationX.value = _slicedTranslateXAnim;
+    } else {
+      if (!pauseTranslation) {
+        translationY.value = translateY;
+        translationX.value = translateX;
+      } else {
+        translationY.value = _stopTranslateYAnim;
+        translationX.value = _stopTranslateXAnim;
+      }
     }
-  }, [animatedUp, gameOver, gamePaused, finalPosition, midPosition]);
+  }, [animatedUp, gameOver, gamePaused, finalPosition, midPosition, sliced]);
 
   const rStyle = useAnimatedStyle(() => {
     return {
